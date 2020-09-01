a9b9a320ce5aabb2daef851df832d5c3abed3645
Natalia Millan <natalia@easypeasyapp.com>
2020-09-01 23:53:05 +0200

    uninstall react-thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,9 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 import phonesReducer from './reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   phonesReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
