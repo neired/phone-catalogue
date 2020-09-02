173ac107058e07887650342195740718a3610be4
Natalia Millan <natalia@easypeasyapp.com>
2020-09-02 23:49:39 +0200

    add flex column to error card

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Error = props => {
   const { error } = props;
   return (
-    <div className="card detail">
+    <div className="card error__card">
       <h2 className="title-secondary title pb-20 text-color-secondary">Oops!</h2>
       <p className="detail__error-tag title text-light title-tertiary">:(</p>
       <p className="text text primary">{error}</p>
